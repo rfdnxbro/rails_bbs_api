1f522c3ebb6f2128c7ed1483467c4c7eb0ed7180
EC2 Default User <ec2-user@ip-172-30-2-78.ap-northeast-1.compute.internal>
2020-09-06 01:47:17 +0000

    change factoryBot

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,20 +6,20 @@ RSpec.describe Post, type: :model do
   describe "subject" do
     context "blankの時に" do
       it "invalidになる" do
-        post = Post.new(subject: "", body: "fuga")
+        post = build(:post, subject: "")
         expect(post).not_to be_valid
       end
     end
     context "maxlengthにより" do
       context "30文字の場合に" do
         it "validになる" do
-          post = Post.new(subject: "あ" * 30, body: "fuga")
+          post = build(:post, subject: "あ" * 30)
           expect(post).to be_valid
         end
       end
       context "31文字の場合に" do
         it "invalidになる" do
-          post = Post.new(subject: "あ" * 31, body: "fuga")
+          post = build(:post, subject: "あ" * 31)
           expect(post).not_to be_valid
         end
       end
